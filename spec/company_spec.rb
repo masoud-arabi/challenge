bcfd2cbcd4a03ee6d0df2049f38a09effb98922c
masoud arabi <applepie@masouds-MacBook-Air.local>
2023-05-05 12:18:12 -0400

    Add good title for controller spec file

--- spec/company_spec.rb
+++ spec/company_spec.rb
@@ -6,6 +6,12 @@ RSpec.describe Company do
       @company = Company.new('./data/companies.json')
       expect(@company.all.size).to eql(5)
     end
+
+    it "returns the number of companies" do
+      @company = Company.new('./data/companies.json')
+      expect(@company.all.first['id']).to eql(1)
+      expect(@company.all.last['id']).to eql(5)
+    end
   end
 end
 
--- spec/controller_spec.rb
+++ spec/controller_spec.rb
@@ -1,8 +1,8 @@
 require 'challenge'
 
 RSpec.describe Controller do 
-  describe "#all" do
-    it "returns the number of companies" do
+  describe "#list_all_info" do
+    it "returns the good text" do
       @controller = Controller.new('./data/companies.json', './data/users.json')
       expect(@controller.list_all_info[1..13]).to eql("Company_id: 1")
     end
